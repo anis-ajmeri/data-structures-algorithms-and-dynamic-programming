6ac034c2d4f32208d6dc7bfc89b3bfd8f7708412
anis-ajmeri <anis.ajmeri2691@gmail.com>
2021-05-12 00:25:52 +0530

    Adding index

--- C/algorithms/others/max_sum_contiguous_subarray_kadane.c
+++ C/algorithms/others/max_sum_contiguous_subarray_kadane.c
@@ -2,18 +2,28 @@
 int main(){
 		int x[] = {10,-12,13,6,-19,63,-3,12,4,32};
 		int size = 10;
-		int lMax,gMax,i,sum;
+		int lMax,gMax,i,sum,si,ei;
 		lMax = x[0];
 		gMax = x[0];
+		si=0;
+		ei=0;
 		i=1;
 		while(i<=size-1)
 		{
 			sum = lMax + x[i];
-			lMax = x[i]>sum?x[i]:sum;
-			gMax = lMax>gMax?lMax:gMax;
+			if(x[i]>sum){
+				lMax = x[i];
+				si = i;
+			}else{
+				lMax = sum;
+			}
+			if(lMax>gMax){
+				gMax = lMax;
+				ei = i;
+			}
 			i++;
 		}
-		printf("%d",gMax);
+		printf("Index(%d->%d) => %d",si,ei,gMax);
 		return 0;
 	
 }
