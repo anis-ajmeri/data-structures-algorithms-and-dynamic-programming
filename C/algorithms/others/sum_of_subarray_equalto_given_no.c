4c18eeadaf66d440d2db72beb74147268537bd4a
anis-ajmeri <anis.ajmeri2691@gmail.com>
2021-05-17 00:10:45 +0530

    Sum of Contiguous SubArray equal to Given number

---
+++ C/algorithms/others/sum_of_subarray_equalto_given_no.c
@@ -0,0 +1,30 @@
+// Sum of contiguous SubArray equal to given number
+#include<stdio.h>
+int main()
+{
+	int x[] = {12,5,6,7,2,8,4};
+	int size = sizeof(x)/sizeof(x[0]);
+	int target,sum;
+	target = 10;
+	int e,f;
+	e=0;
+	while(e<size-1)
+	{
+		sum = 0;
+		f=e;
+		while(f<=size-1)
+		{
+			sum +=x[f];
+			if(sum==target){
+				printf("%d-%d ",e,f);
+				return 0;
+			}
+			if(sum>target) break;
+			f++;
+		}
+		
+		e++;
+	}
+return 0;
+	
+}
